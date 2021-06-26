55fc3d67280c80e3b9b68cce0c5ea7ce76f85024
Lineng Cao <linengca@usc.edu>
2021-06-26 13:55:21 -0700

    add brew path

--- debian.sh
+++ debian.sh
@@ -5,18 +5,19 @@ yes | sudo apt-get install git zsh python3 curl wget python3-pip
 # brew
 echo "======================== brew ========================="
 BREW_INSTALL_COMMAND="$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+BREW_PATH_COMMAND="$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 BREW_DIR="/home/linuxbrew"
 if [ -d "$BREW_DIR" ]; then
   echo "Do you want to reinstall brew? (yes), followed by [ENTER]"
   select yn in "Yes" "No"; do
     case $yn in
-        Yes ) echo "Reinstalling brew start"; sudo rm -rf $BREW_DIR; yes | /bin/bash -c "$BREW_INSTALL_COMMAND"; break;;
+        Yes ) echo "Reinstalling brew start"; sudo rm -rf $BREW_DIR; yes | /bin/bash -c "$BREW_INSTALL_COMMAND"; echo "eval \"$BREW_PATH_COMMAND\"" >> $HOME/.profile; eval "$BREW_PATH_COMMAND"; break;;
         No ) echo "Installing brew abort"; break;;
     esac
   done
 else
   echo "Installing brew start"
-  yes | /bin/bash -c "$BREW_INSTALL_COMMAND"
+  yes | /bin/bash -c "$BREW_INSTALL_COMMAND" && echo "eval \"$BREW_PATH_COMMAND\"" >> $HOME/.profile && eval "$BREW_PATH_COMMAND"
 fi
 
 # n
